730d6926171962bc22d70f339d2bd1c804db5162
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2024-01-22 03:42:38 +0100

    j

--- src/pages/Chats/Chats.tsx
+++ src/pages/Chats/Chats.tsx
@@ -27,7 +27,6 @@ export default function ChatsPage() {
             firstName: "Muhammed",
             lastName: "Kaplan",
             profileImageRef: "null",
-            userId: "",
           }}
           onClickDelete={() => null}
         />
