d995c98595b966289e9ad6b60dbf9702b7d9b6d2
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2024-01-27 02:28:04 +0100

    fix(settings): add header

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,9 +1,13 @@
+import AppHeader from "@/components/App/AppHeader";
 import SettingsModule from "@/modules/settings/settings.module";
-import { IonContent, IonPage } from "@ionic/react";
+import { IonContent, IonPage, IonTitle } from "@ionic/react";
 
 export default function SettingsPage() {
   return (
     <IonPage>
+      <AppHeader withBackButton>
+        <IonTitle>Settings</IonTitle>
+      </AppHeader>
       <IonContent>
         <SettingsModule />
       </IonContent>
