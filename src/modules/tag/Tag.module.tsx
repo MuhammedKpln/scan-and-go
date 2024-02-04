8a2c3c7b830d37afd738b93cf70ae56a0e1382ca
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2024-02-04 17:41:12 +0100

    fix: show user profile picture

--- src/modules/tag/Tag.module.tsx
+++ src/modules/tag/Tag.module.tsx
@@ -1,9 +1,11 @@
 import AppLoading from "@/components/App/AppLoading";
+import { NO_AVATAR_IMAGE } from "@/constants";
 import { QueryKeys } from "@/models/query_keys.model";
 import { TagDetailPageProps } from "@/pages/Tag/Tag";
 import styles from "@/pages/Tag/Tag.module.scss";
 import { noteService } from "@/services/note.service";
 import { profileService } from "@/services/profile.service";
+import { storageService } from "@/services/storage.service";
 import { tagService } from "@/services/tag.service";
 import {
   IonButton,
@@ -24,12 +26,13 @@ import {
   mailOpenOutline,
   phonePortraitOutline,
 } from "ionicons/icons";
-import { useCallback, useMemo } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import SendMessageModule from "./send_message.module";
 
 export default function TagModule(props: TagDetailPageProps) {
   const [showAlert] = useIonAlert();
   const router = useIonRouter();
+  const [profilePicture, setProfilePicture] = useState<string>(NO_AVATAR_IMAGE);
 
   const fetchTag = useCallback(() => {
     return tagService.fetchTag(props.match.params.tagUid);
@@ -129,6 +132,20 @@ export default function TagModule(props: TagDetailPageProps) {
   );
   const phoneData = useMemo(() => phoneQuery.data?.data(), [phoneQuery]);
 
+  useEffect(() => {
+    async function loadAvatar() {
+      if (profileData?.profileImageRef) {
+        const image = await storageService.getAvatar(
+          profileData.profileImageRef
+        );
+
+        setProfilePicture(image);
+      }
+    }
+
+    loadAvatar();
+  }, [profileData]);
+
   if (tagQuery.isLoading || profileQuery.isLoading) {
     return <AppLoading message="Loading tag..." />;
   }
@@ -150,10 +167,7 @@ export default function TagModule(props: TagDetailPageProps) {
   return (
     <div className={styles.container}>
       <div id="userDetails" className="">
-        <img
-          src={profileData?.profileImageRef}
-          className="w-32 h-32 rounded-full"
-        />
+        <img src={profilePicture} className="w-32 h-32 rounded-full" />
 
         <h1>
           {profileData?.firstName} {profileData?.lastName}
