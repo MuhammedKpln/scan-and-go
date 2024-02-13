a9cc31f458faec6bce7e9ad4fc74f24b0904ce9e
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2024-02-13 12:21:44 +0100

    r

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,9 +4,6 @@ const config: CapacitorConfig = {
   appId: "com.muhammedkpln.scango",
   appName: "Scan & Go",
   webDir: "dist",
-  server: {
-    androidScheme: "https",
-  },
   plugins: {
     PushNotifications: {
       presentationOptions: ["badge", "sound", "alert"],
