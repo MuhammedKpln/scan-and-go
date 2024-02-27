5a253c3deed3c7dfb488ec3ca06959dc009d8f99
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2024-02-27 15:16:22 +0100

    fix: loading screen does not removes

--- src/hooks/app/useSplashScreen.ts
+++ src/hooks/app/useSplashScreen.ts
@@ -1,13 +1,21 @@
 import { useAuthStore } from "@/stores/auth.store";
 import { SplashScreen } from "@capacitor/splash-screen";
+import { useIonLoading } from "@ionic/react";
 import { useEffect } from "react";
 
 export function useSplashScreen() {
   const isInitialized = useAuthStore((state) => state.isInitialized);
+  const [showLoading, hideLoading] = useIonLoading();
 
   useEffect(() => {
     if (isInitialized) {
       SplashScreen.hide();
+      hideLoading();
     }
   }, [isInitialized]);
+
+  return {
+    showLoading,
+    hideLoading,
+  };
 }
