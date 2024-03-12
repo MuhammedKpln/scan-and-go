1a5a9a57f38ab904d8a4f52c9976cf0317a5e318
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2024-03-12 04:44:44 +0100

    supabase migrations

--- supabase/migrations/20240304141654_auth_profile_trigger.sql
+++ supabase/migrations/20240304141654_auth_profile_trigger.sql
@@ -1,4 +1,4 @@
 -- trigger the function every time a user is created
-create trigger on_auth_user_created
+create or replace trigger on_auth_user_created
   after insert on auth.users
   for each row execute procedure public.handle_new_user();
